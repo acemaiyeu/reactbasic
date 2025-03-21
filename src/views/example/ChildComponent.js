ace66a2e4c66b9761b914778906bb182c4a3dad7
Huy FE <nthanhhuy82@gmail.com>
2025-03-21 16:35:24 +0700

    connect with scss on Component

--- src/views/example/ChildComponent.js
+++ src/views/example/ChildComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Demo.scss";
 class ChildComponent extends React.Component {
   state = {
     isShow: false,
@@ -18,7 +19,9 @@ class ChildComponent extends React.Component {
       <>
         {isShow === false ? (
           <div>
-            <button onClick={() => this.handleShowHide()}>Show</button>
+            <button onClick={() => this.handleShowHide()} className="btn-show">
+              Show
+            </button>
           </div>
         ) : (
           <>
